f265f5ff656d0a024c9eaf1906e909ecbcd58ed3
NahiyanAziz <116001101+NahiyanAziz@users.noreply.github.com>
2022-10-25 07:56:28 +0600

    Update Lab 2 q3.cpp

--- Lab 2 q3.cpp
+++ Lab 2 q3.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
-
+#include <bits/stdc++h>
 using namespace std;
 
 int main()
 {
-
     int a[6],c=0;
-
     cout<<"Please enter six numbers into the array: "<<endl;
     for(int i=0;i<6;i++){
         cin>>a[i];
-    }
-
+  }
     for(int i=0; i<6;i++){
         for(int j=0;j<6;j++){
             if(j==i){
